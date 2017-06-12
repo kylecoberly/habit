3a2d3b1f7a095ee21e9155df14a5f9023600e6be
Kyle Coberly <kyle.coberly@gmail.com>
2017-06-11 23:07:27 -0600

    Adding manifest

---
+++ config/manifest.js
@@ -0,0 +1,16 @@
+/*jshint node:true*/
+"use strict";
+
+module.exports = function(/* environment, appConfig */) {
+	return {
+		name: "Habit",
+		short_name: "Habit",
+		description: "Keeping track of Kyle's habits",
+		start_url: "/",
+		display: "standalone",
+		background_color: "66cc66",
+		theme_color: "66cc66",
+		icons: [
+		]
+	};
+}
